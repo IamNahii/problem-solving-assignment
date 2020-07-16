029d2004ac55a61d0ef66b2a5c29649333430a2d
IamNahii <jcpsc.nahid@gmail.com>
2020-07-17 04:20:25 +0600

    woodCalculator added

--- assignment.js
+++ assignment.js
@@ -7,3 +7,17 @@ function feetToMile(feet){
 var mileIs = feetToMile(15);
 mileIs = mileIs.toFixed(8);
 console.log("Miles is:", mileIs);
+
+//woodCalculator
+
+function woodCalculator(table, chair, bed){
+    var tableWoodNumber = table * 10;
+    var chairWoodNumber = chair * 5;
+    var bedWoodNumber = bed * 20;
+    var totalWoodNumber = tableWoodNumber + chairWoodNumber + bedWoodNumber;
+    return totalWoodNumber;
+}
+var numberOfTableChairBed = woodCalculator(5, 10, 5);
+console.log(numberOfTableChairBed);
+
+//brickCalculator
