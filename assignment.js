18b73548c1532c24ba7d99ad41b8a611558a8913
IamNahii <jcpsc.nahid@gmail.com>
2020-07-17 23:21:33 +0600

    finished

--- assignment.js
+++ assignment.js
@@ -11,13 +11,47 @@ console.log("Miles is:", mileIs);
 //woodCalculator
 
 function woodCalculator(table, chair, bed){
-    var tableWoodNumber = table * 10;
-    var chairWoodNumber = chair * 5;
-    var bedWoodNumber = bed * 20;
+    var tableWoodNumber = table * 3;
+    var chairWoodNumber = chair * 1;
+    var bedWoodNumber = bed * 5;
     var totalWoodNumber = tableWoodNumber + chairWoodNumber + bedWoodNumber;
     return totalWoodNumber;
 }
 var numberOfTableChairBed = woodCalculator(5, 10, 5);
-console.log(numberOfTableChairBed);
+console.log("Wood needed:", numberOfTableChairBed, "cubic feet");
 
 //brickCalculator
+
+function brickCalculator(floorNo){
+    var oneFeet = 1000;
+    if(floorNo <= 10){
+        var bricksNumber = ((15 * oneFeet) * floorNo);
+        return bricksNumber;
+    }
+    
+    else if( floorNo >= 11 && floorNo <=20){
+        bricksNumber = ((150000) + ((floorNo - 10) * 12000));
+        return bricksNumber;
+    }
+    else if( floorNo >= 21){
+        bricksNumber = ((150000 + 120000) + ((floorNo - 20) * 10000));
+        return bricksNumber;
+    }
+}
+
+var total = brickCalculator(27);
+console.log(total);
+
+
+//tinyFriend
+
+function tinyFriend(friendsName){
+    var tinyName = friendsName[0];
+    for(var i = 0; i < friendsName.length; i++){
+        var currentName = friendsName[i];
+        if(currentName < tinyName){
+            tinyName = currentName;
+        }
+    }
+    return tinyName;
+}
